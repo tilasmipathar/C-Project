167cc29da48a655adb0cea788b5f4bba5103d857
ishaanj0412 <77500206+ishaanj0412@users.noreply.github.com>
2022-01-06 23:10:33 +0530

    Add files via upload

---
+++ brickbreaker/ball.hpp
@@ -0,0 +1,43 @@
+/*Ball class*/
+#ifdef __gnu_linux__
+#include "SDL2/SDL.h"
+#include "SDL2/SDL_image.h"
+#include "SDL2/SDL_ttf.h"
+#endif
+
+//Vector
+typedef struct
+{
+    int x, y;
+}vect;
+
+//Position and velocity vector
+typedef struct 
+{
+    vect* pos, *vel;
+}pos;
+
+//Texture of ball
+typedef struct
+{
+    double width, height;
+    SDL_Rect* ballrect;
+}texture;
+
+//Ball class
+class ball
+{
+    private:
+        pos* ball_pos;
+        texture* balltext;
+
+    public:
+        ball(int height, int width, int xcor, int ycor, int vxinit, int vyinit);
+        void updatevel(SDL_Rect* paddle);
+        void updatecord();
+        pos* getpos();
+        texture* gettexture();
+};
+
+
+ 
